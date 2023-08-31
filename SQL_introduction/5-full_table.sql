82f5db359835a069958dc22cb0daa322506cd3ba
Ajala <supremeajala@gmail.com>
2023-08-31 02:26:22 +0100

    sixth task

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- This script prints the full description of tthe table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-  AND TABLE_NAME = 'first_table';
+-- This script prints the full description of the table
+SHOW table first_table; 
