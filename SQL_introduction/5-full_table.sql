42a1235123841e5303697c19b40e6a18bc7ddfe2
Ajala <supremeajala@gmail.com>
2023-08-31 20:16:20 +0100

    sixth task

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- This script prints the full description of the table
--- CREATE DATABASE hbtn_0c_0;
-USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
 SHOW CREATE TABLE first_table;
