00e04aa88e2069555370e63dbd9f6fdf57f60c11
Ajala <supremeajala@gmail.com>
2023-09-05 23:05:04 +0100

    first task

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- This script lists all privileges of the users
-SHOW GRANTS FOR 'user_0d_1'@"loacalhost";
-SHOW GRANTS FOR 'user_0d_2'@"loacalhost";
+SHOW GRANTS FOR 'user_0d_1'@"localhost";
+SHOW GRANTS FOR 'user_0d_2'@"localhost";
