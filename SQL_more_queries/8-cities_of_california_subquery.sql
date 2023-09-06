91336bce68c0c23a77eb47408c0c7b3a0ea96ef0
Ajala <supremeajala@gmail.com>
2023-09-06 15:11:05 +0100

    ninth task

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- This script lists all the cities of california on this database
+USE hbth_0d_usa;
+
+-- Find the state_id for california 
+SELECT id INTO @california_state_id FROM states name = 'California';
+
+-- List all cities of california 
+SELECT cities.name
+FROM cities
+WHERE cities.state_id = @california_state_id
+ORDER BY cities.id ASC;
