c2ce598dea680795a14127c560bc65d42ebe55d5
Ajala <supremeajala@gmail.com>
2023-09-06 15:27:16 +0100

    NINTH TASK

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,6 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
+CREATE TABLE states (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+
 -- Find the state_id for california 
 SELECT id INTO @california_state_id FROM states WHERE name = 'California';
 
