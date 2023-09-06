ecfc8cb3a1e7cbe3a7838277267062b7b465c93d
Ajala <supremeajala@gmail.com>
2023-09-06 15:50:46 +0100

    ninth task

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,17 +1,6 @@
 -- This script lists all the cities of california on this database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-USE hbtn_0d_usa;
-
-CREATE TABLE states (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
-
--- Find the state_id for california 
-SELECT id INTO @california_state_id FROM states WHERE name = 'California';
-
-CREATE TABLE cities (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
-
--- List all cities of california 
-SELECT cities.name
-FROM cities
-WHERE cities.state_id = @california_state_id
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+AND states.name = 'California'
 ORDER BY cities.id ASC;
