0b4915a14115bb414b0c659c5d248a3294daf69d
Ajala <supremeajala@gmail.com>
2023-09-06 16:01:34 +0100

    LAST TASK

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- This script lists all cities in the database
 
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name AS name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
